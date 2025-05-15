fba105904581a73725184bb1009a9e429e564f1c
erikplol <terrydityo@gmail.com>
2025-05-15 19:27:12 +0700

    tugas 3

--- tugas-3/file_client_cli.py
+++ tugas-3/file_client_cli.py
@@ -3,7 +3,8 @@ import json
 import base64
 import logging
 
-server_address=('0.0.0.0',7777)
+server_address = ('0.0.0.0', 7777)
+
 
 def send_command(command_str=""):
     global server_address
@@ -13,59 +14,86 @@ def send_command(command_str=""):
     try:
         logging.warning(f"sending message ")
         sock.sendall(command_str.encode())
-        # Look for the response, waiting until socket is done (no more data)
-        data_received="" #empty string
+        data_received = ""  # empty string
         while True:
-            #socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
             data = sock.recv(16)
             if data:
-                #data is not empty, concat with previous content
                 data_received += data.decode()
                 if "\r\n\r\n" in data_received:
                     break
             else:
-                # no more data, stop the process by break
                 break
-        # at this point, data_received (string) will contain all data coming from the socket
-        # to be able to use the data_received as a dict, need to load it using json.loads()
         hasil = json.loads(data_received)
         logging.warning("data received from server:")
         return hasil
-    except:
-        logging.warning("error during data receiving")
+    except Exception as e:
+        logging.warning(f"error during data receiving: {e}")
         return False
 
 
 def remote_list():
-    command_str=f"LIST"
+    command_str = f"LIST\r\n"
     hasil = send_command(command_str)
-    if (hasil['status']=='OK'):
+    if hasil and hasil['status'] == 'OK':
         print("daftar file : ")
         for nmfile in hasil['data']:
             print(f"- {nmfile}")
         return True
     else:
-        print("Gagal")
+        print("Gagal mendapatkan daftar file")
         return False
+
 
 def remote_get(filename=""):
-    command_str=f"GET {filename}"
+    command_str = f"GET {filename}\r\n"
     hasil = send_command(command_str)
-    if (hasil['status']=='OK'):
-        #proses file dalam bentuk base64 ke bentuk bytes
-        namafile= hasil['data_namafile']
+    if hasil and hasil['status'] == 'OK':
+        namafile = hasil['data_namafile']
         isifile = base64.b64decode(hasil['data_file'])
-        fp = open(namafile,'wb+')
-        fp.write(isifile)
-        fp.close()
+        with open(namafile, 'wb+') as fp:
+            fp.write(isifile)
+        print(f"File '{namafile}' berhasil di-download.")
         return True
     else:
-        print("Gagal")
+        print("Gagal mendapatkan file")
         return False
 
 
-if __name__=='__main__':
-    server_address=('172.16.16.101',6666)
+def remote_upload(local_filename="", remote_filename=""):
+    try:
+        with open(local_filename, "rb") as f:
+            file_content = f.read()
+        encoded_content = base64.b64encode(file_content).decode()
+        command_str = f"UPLOAD {remote_filename} {encoded_content}\r\n"
+        hasil = send_command(command_str)
+        if hasil and hasil['status'] == 'OK':
+            print(f"File '{local_filename}' berhasil di-upload sebagai '{remote_filename}'.")
+            return True
+        else:
+            print("Gagal upload file:", hasil.get('data', 'Unknown error'))
+            return False
+    except FileNotFoundError:
+        print(f"File '{local_filename}' tidak ditemukan.")
+        return False
+
+
+def remote_delete(remote_filename=""):
+    command_str = f"DELETE {remote_filename}\r\n"
+    hasil = send_command(command_str)
+    if hasil and hasil['status'] == 'OK':
+        print(f"File '{remote_filename}' berhasil dihapus dari server.")
+        return True
+    else:
+        print("Gagal menghapus file:", hasil.get('data', 'Unknown error'))
+        return False
+
+
+if __name__ == '__main__':
+    server_address = ('172.16.16.101', 6666)
+
     remote_list()
     remote_get('donalbebek.jpg')
-
+    remote_upload('test.txt', 'upload_test.txt')
+    remote_list()
+    remote_get('upload_test.txt')
+    remote_delete('upload_test.txt')
